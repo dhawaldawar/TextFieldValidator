bdc3bc244b5e610a2b4c3ae3bc8fee1638f51da2
trykov <me@trykov.ru>
2016-12-24 14:59:12 +0300

    presentInView retain cycle

--- TextFieldValidator/TextFieldValidator.h
+++ TextFieldValidator/TextFieldValidator.h
@@ -53,7 +53,7 @@ NS_CLASS_AVAILABLE_IOS(6_0) @interface TextFieldValidator : UITextField<UITextFi
 
 @property (nonatomic,assign) BOOL isMandatory;   /**< Default is YES*/
 
-@property (nonatomic,retain) IBOutlet UIView *presentInView;    /**< Assign view on which you want to show popup and it would be good if you provide controller's view*/
+@property (nonatomic,weak) IBOutlet UIView *presentInView;    /**< Assign view on which you want to show popup and it would be good if you provide controller's view*/
 
 @property (nonatomic,retain) UIColor *popUpColor;   /**< Assign popup background color, you can also assign default popup color from macro "ColorPopUpBg" at the top*/
 
